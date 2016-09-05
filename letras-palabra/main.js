adb9bc17111da11dae1da37623cc0320ca48a56d
anbelespinal <anabelespinal>
2016-09-05 10:05:21 -0500

    buscando letras

--- letras-palabra/main.js
+++ letras-palabra/main.js
@@ -1,28 +1,10 @@
-/*
-var palabra = prompt("INSERTE UNA PALABRA");
-var letra = prompt("¿QUÉ LETRA BUSCA?");
-
-for(var i = 0; i < palabra.length; i++){
-	console.log(palabra.charAt(i));
-}
-var palabra= prompt("INSERTE UNA PALABRA");
-var letra = prompt("¿QUÉ LETRA BUSCA?");
-
-for(i = 0; i < palabra.length; i++){
-	if(palabra.charAt(i) == letra){
-		letra++;
-		document.write("Tiene" + letra); 
-	}
-}
-*/
-
 window.addEventListener("load", function() {
-	var boton = document.getElementById("calcular");
+	var boton = document.getElementById("contar");
 	boton.addEventListener("click", function() {
 	var palabra = document.getElementById("palabra").value;
 	var letra = document.getElementById("letra").value;
 
-	var resultado = document.getElementById("resultado");
+	var resultado = document.getElementById("contador");
 	resultado.innerHTML = "<p>La letra " + letra + " se muestra " + letrasIguales(palabra, letra) + " vez/veces en " + palabra + "</p>";
 	});
 
